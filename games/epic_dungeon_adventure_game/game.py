8e4b8bfac47d8b920ed92044e52c2e8af102da0a
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-06-21 22:17:11 +0300

    nermen

--- games/epic_dungeon_adventure_game/game.py
+++ games/epic_dungeon_adventure_game/game.py
@@ -7,15 +7,15 @@ from components.sound import Sound
 from .animations import get_animations
 
 pygame.init()
-screen = pygame.display.set_mode((1300,600))
+screen = pygame.display.set_mode((1600,800))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
 animations = get_animations()
 game_state = "walk"
 
-head = Entity(animations['demon']['idle'])
-head.animate(animations['demon']['cleave'])
+head = Entity(animations['ice_boss']['idle'])
+head.animate(animations['ice_boss']['attack'])
 head_group = pygame.sprite.Group()
 head_group.add(head)
 head.animation_speed *= 1.5
@@ -26,16 +26,16 @@ posy = 50
 
 
 
-class MainCharacter():
-    def __init__(self, default_animation):
-        self.entity = Entity(default_animation)
+# class MainCharacter():
+#     def __init__(self, default_animation):
+#         self.entity = Entity(default_animation)
 
     
 
 
-sound = Sound("./games/epic_dungeon_adventure/assets/game_sound.wav")
+# sound = Sound("./games/epic_dungeon_adventure/assets/game_sound.wav")
 
-sound.play()
+# sound.play()
 
 
 text = "In the depths of a frozen cavern, amidst towering ice walls and glittering icicles, an awe-inspiring ice dragon awaits your arrival. Its colossal body, adorned with shimmering scales of ice, emanates an intense coldness that permeates the chamber. As the dragon fixes its piercing gaze upon you, its voice resonates with ancient wisdom, questioning your purpose in its icy domain. With a mixture of wonder and trepidation, your fate becomes intertwined with this majestic creature, as you stand on the threshold of a chilling and thrilling adventure."
