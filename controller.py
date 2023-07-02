3be0e0a083252cbeae06fd43b53d6fbc25812c63
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-07-02 17:41:36 +0300

    save

--- controller.py
+++ controller.py
@@ -1,5 +1,20 @@
-from games.epic_dungeon_adventure_game.game import play
 
-#GUI
 
-play()
+chosen_game = None
+def chose_game(name):
+    global chosen_game
+    if name == "epic dungeon adventure":
+        from games.epic_dungeon_adventure_game.game import play
+    if name == "flappy bird":
+        from games.flappy_bird.game import play
+    if name == "whack mole":
+        from games.whack_mole.game import play
+    chosen_game = play
+
+
+name = "epic dungeon adventure"
+
+chose_game(name)
+
+chosen_game()
+
